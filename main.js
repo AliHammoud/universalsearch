c372623e0353be4a65073150211069c9566f3fcf
Ali Hammoud <ali.hammoud.lb@gmail.com>
2017-05-17 11:38:09 +0300

    Implemented universal searchbar

---
+++ main.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+  'use strict';
+
+  var content = [
+    {
+      title: 'Manage Departments',
+      url: 'https://projects.invisionapp.com/d/main#/console/10822833/232987376/preview',
+      description: 'Get a full overview over your organization\'s departments'
+    },
+    {
+      title: 'Financials',
+      description: 'See how well your organization is doing'
+    },
+    {
+      title: 'Manage Users',
+      description: 'Invite, block, delete users'
+    },
+    {
+      title: 'make coffee',
+      url: './coffee.html',
+      description: 'We will ask Osama to make everyone a cup of coffee'
+    },
+    {
+      title: 'visit website',
+      url: 'www.bi.care',
+      description: 'Visit our corporate website to get to know more about us and our products'
+    },
+    {
+      title: 'upload data',
+      url: 'https://projects.invisionapp.com/d/main#/console/10822833/232987375/preview',
+      description: 'Update the system\'s records by uploading a CSV, Excel, or TXT files'
+    },
+    {
+      title: 'create new adhoc chart',
+      url: 'https://projects.invisionapp.com/d/main#/console/10822833/230404111/preview',
+      description: 'Start a new chart'
+    }
+  ];
+
+  $('.ui.search').search({
+    source: content,
+    searchFields: [
+      'title'
+    ],
+    fuzzySearch: true
+  });
+
+});
